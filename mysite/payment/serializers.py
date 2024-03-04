4cf8f2e3b7395a73b7c80deb8b569f648f372926
Светлана Скобельцына <skobsvetlana@gmail.com>
2024-03-04 13:53:34 +0300

    payment/ serializers.py views.py urls.py modified (payment validation created) commit

--- mysite/payment/serializers.py
+++ mysite/payment/serializers.py
@@ -2,14 +2,23 @@ from rest_framework import serializers
 
 from payment.models import Payment
 
+def validate_card_number(number):
+    if len(number) > 16:
+        raise serializers.ValidationError("Card number must not be longer than 16 digits.")
+    if int(number) % 2 != 0 or int(number[-2:-1]) == 0:
+        raise serializers.ValidationError("Card number must be even and last digit must be more than 0.")
+    return number
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+        extra_kwargs = {
+            'number': {'validators': [validate_card_number]},
+        }
 
 
     def create(self, validated_data):
         payment = Payment.objects.create(**validated_data)
-        print("payment **validated_data", **validated_data)
         return payment
--- mysite/payment/views.py
+++ mysite/payment/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -15,39 +13,21 @@ class PaymentViewset(ModelViewSet):
     queryset = Payment.objects.all()
 
     def create(self, request: Request, *args, **kwargs) -> Response:
-        # order_id = self.request.session.get('order_id')
-        # payment_id = self.request.session['payment_id']
-        print(request.data)
-        # try:
+        order_id = kwargs.get("id")
         data = request.data
+        data["order"] = order_id
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save()
+        order = Order.objects.get(pk=order_id)
+        order.status = "paid"
+        order.save()
+        request.session['order_id'] = None
 
         return Response(
             data=serializer.data,
-            status=status.HTTP_200_OK,
+            status=status.HTTP_201_CREATED,
         )
-        # except:
-        #     print("there is not payment data")
 
-
-    def perform_create(self, serializer):
-        # Создание платежа и его связь с заказом
-        order_id = self.request.kwargs.get("id")
-        order = Order.objects.get_object_or_404(pk=order_id)
-        validated_data = serializer.validated_data
-        validated_data['order'] = order
-        print("perform_create validated_data", validated_data)
-        serializer.save()
-
-
-    # def retrieve(self, request: Request, *args, **kwargs) -> Response:
-    #
-    #     payment, created = Payment.objects.get_or_create(customer=request.user)
-    #
-    #     serializer = self.get_serializer(payment)
-    #
-    #     return Response(serializer.data)
 
 
