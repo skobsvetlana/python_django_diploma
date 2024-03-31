439e2b7c40f7d64b17b892014b4e18898cd0a5e3
Светлана Скобельцына <skobsvetlana@gmail.com>
2024-03-31 14:48:59 +0300

    mysite/urls.py midified commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
-DEBUG = 1
+DEBUG = getenv("DJANGO_DEBUG", "0") == "1"
+
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "0.0.0.0,"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -31,12 +31,12 @@ urlpatterns = [
     path('api/', include("my_auth.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns.extend(
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
 
-    urlpatterns.extend(
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    )
+urlpatterns.extend(
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+urlpatterns.extend(
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
